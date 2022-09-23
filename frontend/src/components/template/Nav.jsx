722af30693dd8b7c025207c5898c7d442d9f42e3
GuiTh <guilhermetcarlos@hotmail.com>
2022-09-23 12:56:12 -0300

    att crud

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        {/* refatorar em casa, separar cada icone do navegador como um componente separado  */}
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i>Inicio
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuarios
+            </a>
+        </nav>
     </aside>
