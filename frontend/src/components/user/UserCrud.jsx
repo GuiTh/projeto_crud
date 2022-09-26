19fb404250896907bce637d4ca0b39de732309ea
GuiTh <guilhermetcarlos@hotmail.com>
2022-09-25 21:54:03 -0300

    att fim do frontend

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -17,6 +17,14 @@ export default class UserCrud extends Component{
 
     state= {...initialState}
 
+    componentWillMount(){
+        axios(baseUrl)
+        .then(resp =>{ 
+            this.setState({list:resp.data})
+        })
+
+    }
+
     clear(){
         //limpa apenas o usuario
         this.setState({user: initialState.user})
@@ -31,15 +39,113 @@ export default class UserCrud extends Component{
                 this.setState({user: initialState.user, list})
             })
     }
-    getUpdatedList(user){
+    getUpdatedList(user, add = true){
         const list = this.state.list.filter(u => u.id !== user.id)
-        list.unshift(user)
+        if(add) list.unshift(user)
         return list
+    }
+    updateField(event){
+        //clona o usuario
+        const user = {...this.state.user}
+        /*seleciona o atributo do user clicado, e seta nesse atributo o valor que esta dentro do campo de input*/
+        user[event.target.name] = event.target.value
+        this.setState({user})
+    }
+    renderForm(){
+        //jsx que renderiza o formulario
+        return(
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name='name'
+                                value={this.state.user.name}
+                                onChange={e=> this.updateField(e)}
+                                placeholder="Digite o nome..."/>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label htmlFor="">E-mail</label>
+                            <input type="text" className="form-control"
+                                name='email'
+                                value={this.state.user.email} 
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o e-mail..."/> 
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                        onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+                        <button className="btn btn-secundary ml-2"
+                        onClick={e=> this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    load(user){
+        this.setState({user})
+    }
+    remove(user){
+        axios.delete(`${baseUrl}/${user.id}`).then(resp =>{
+            const list = this.getUpdatedList(user, false)
+            this.setState({list})
+        })
+    }
+    renderTable(){
+        return(
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Ids</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+    renderRows(){
+        return this.state.list.map(user => {
+            return(
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning"
+                        onClick={() => this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2"
+                        onClick={() => this.remove(user)}>
+                           <i className="fa fa-trash"></i>  
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
     }
     render(){
         return(
             <Main {...headerProps}>
-                Cadastro de Usuário
+                {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
--- frontend/src/main/Route.jsx
+++ frontend/src/main/Route.jsx
@@ -7,7 +7,7 @@ import UserCrud from '../components/user/UserCrud'
 export default props =>
 
 <Routes>
-    <Route exact path='/' element={Home}/>
-    <Route path='/users' element={UserCrud}/> 
+    <Route exact path='/' element={<Home/>}/>
+    <Route path='/users' element={<UserCrud/>}/> 
     <Route path='*' element={<Navigate to='/'/>}/>
 </Routes>
